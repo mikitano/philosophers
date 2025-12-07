1b7f1eb2376f7cc9c99907695875237f070eb1a6
Magali Midori Kitano <mkitano@c1r5p2.42sp.org.br>
2025-12-07 20:02:18 -0300

    started main and utils

---
+++ philo/src/utils.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mkitano <mkitano@student.42sp.org.br>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/07 16:19:07 by mkitano           #+#    #+#             */
+/*   Updated: 2025/12/07 17:47:38 by mkitano          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+long	ft_atol(const char *str)
+{
+	int		i;
+	int		signal;
+	long	result;
+
+	i = 0;
+	signal = 1;
+	result = 0;
+	while ((str[i] >= '\t' && str[i] <= '\r') || (str[i] == ' '))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			signal = -1;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = (result * 10) + (str[i] - '0');
+		i++;
+	}
+	return (signal * result);
+}
